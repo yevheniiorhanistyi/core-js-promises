3b2197154725724469fbca4487aaddc2af05dbf6
Yevhenii Orhanistyi <organistiy@gmail.com>
2025-04-08 18:40:49 +0200

    feat: implement getFirstResolvedPromiseResult function

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -56,8 +56,10 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return Promise.race(promises)
+    .then((result) => result)
+    .catch(() => Promise.reject());
 }
 
 /**
